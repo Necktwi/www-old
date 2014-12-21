91e1e7a9c0bdf85be4d1373339e64e226ca5736e
Satya Gowtham Kudupudi <satyagowtham.k@gmail.com>
2014-12-21 16:39:14 +0530

    links are being implemented in base library

--- js/main.js
+++ js/main.js
@@ -4,24 +4,37 @@ var Engine = famous.core.Engine;
 var Modifier = famous.core.Modifier;
 var Transform = famous.core.Transform;
 var ImageSurface = famous.surfaces.ImageSurface;
+var ModifierChain = famous.modifiers.ModifierChain;
 
 // create the main context
 var mainContext = Engine.createContext();
 
 // your app here
 var logo = new ImageSurface({
-    size: [200, 200],
-    content: 'img/FerryFairLogo1.svg',
-    classes: ['double-sided']
+	size: [200, 200],
+	content: 'img/FerryFairLogo1.svg',
+	classes: ['double-sided']
 });
+
+var spinModifierChain = new ModifierChain();
 
 var initialTime = Date.now();
-var centerSpinModifier = new Modifier({
-    origin: [0.5, 0.5],
-    align: [0.5, 0.5],
-    transform : function () {
-        return Transform.rotateY(.001 * (Date.now() - initialTime));
-    }
+
+var centerModifier = new Modifier({
+	origin: [0.5, 0.5],
+	align: [0.5, 0.5]
 });
 
-mainContext.add(centerSpinModifier).add(logo);
+var spinYModifier = new Modifier({
+	transform: function () {
+		return Transform.rotateY(.001 * (Date.now() - initialTime));
+	}
+});
+
+var spinXModifier = new Modifier({
+	transform: function () {
+		return Transform.rotateX(.001 * (Date.now() - initialTime));
+	}
+});
+
+mainContext.add(centerModifier).add(spinXModifier).add(spinYModifier).add(logo);
