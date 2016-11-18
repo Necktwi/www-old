ada3c95e6c2c2e14af138fbf2e2b819e8074c67d
Gowtham Kudupudi <necktwi@icloud.com>
2016-11-18 19:14:15 +0530

    no rotation about x axis

---
+++ js/main.js
@@ -0,0 +1,40 @@
+/*global famous*/
+// import dependencies
+var Engine = famous.core.Engine;
+var Modifier = famous.core.Modifier;
+var Transform = famous.core.Transform;
+var ImageSurface = famous.surfaces.ImageSurface;
+var ModifierChain = famous.modifiers.ModifierChain;
+
+// create the main context
+var mainContext = Engine.createContext();
+
+// your app here
+var logo = new ImageSurface({
+	size: [200, 200],
+	content: 'img/FerryFairLogo1.svg',
+	classes: ['double-sided']
+});
+
+var spinModifierChain = new ModifierChain();
+
+var initialTime = Date.now();
+
+var centerModifier = new Modifier({
+	origin: [0.5, 0.5],
+	align: [0.5, 0.5]
+});
+
+var spinYModifier = new Modifier({
+	transform: function () {
+		return Transform.rotateY(.001 * (Date.now() - initialTime));
+	}
+});
+
+var spinXModifier = new Modifier({
+	transform: function () {
+		return Transform.rotateX(.001 * (Date.now() - initialTime));
+	}
+});
+
+mainContext.add(centerModifier).add(spinYModifier).add(logo);
